6166074f7038a78a4985979b59253da74bdc1870
MarinBagrin <marinbagrin@outlook.com>
2025-09-12 23:55:16 +0300

    Initial Commit

---
+++ MBBank24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MBBank24
+//
+//  Created by Marin Bagrin on 12.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
